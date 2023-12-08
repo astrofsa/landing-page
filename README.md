0ef9b78917cac46addce77824641731bdde1063b
ASTRO at FSA <astro@fultonscienceacademy.org>
2023-12-08 18:11:02 -0500

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# landing-page
-ASTRO Landing Page while main get's developed!
+# ASFSA Website Landing Page
+This is ASFSA! We are Astronomy Society of Fulton Science Academy. 
+This would be the early landing page as while the main page is getting developed.
+
+# Pull Requests
+This website is not accepting pull requests at this point. 
+If you have any inquiry/suggestions, please contact [the webmaster](mailto://ykim+astroweb@fultonscienceacademy.org).
+
+# LICENSE
+Will be updated
+
+# Acknowledgement
+This website has utilized [Next.js Boilerplate](https://github.com/vercel/vercel/tree/main/examples/nextjs) pre-developed modules developed by [Vercel](https://github.com/vercel). Visit more information at [here](https://vercel.com/templates).
+This website will be only utilized during we update our content in our main page. 
+
+# Getting Started
+```
+1. npm install -D
+2. npm run dev
+```
+\3. Visit [http://localhost:3000](httplocalhost:3000) with your browser to see the result.
+
+This project uses next/font to automatically optimize and load Inter, a custom Google Font.
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
+          Hold tight as we are working to produce the most astonishing product ever.
+          
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -15,15 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            By <a href='https://fultonsciecneacademy.org'>Fulton Science Academy</a>
           </a>
         </div>
       </div>
@@ -47,13 +39,13 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Scripta{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Student-run academic journal
           </p>
         </a>
 
@@ -64,7 +56,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Awards{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
